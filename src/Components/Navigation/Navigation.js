38f20b317c0c27465fb5a6e878bd496e785247f5
108nandita <108nandita@gmail.com>
2022-12-06 16:24:54 +0100

    Zo ver ik kon komen

---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <nav>
+            <div className="nav-container">
+              <ul>
+                  <li>Home</li>
+                  <li>Blogposts</li>
+                  <li>Login</li>
+              </ul>
+
+            </div>
+        </nav>
+    );
+};
+
+export default Navigation;
